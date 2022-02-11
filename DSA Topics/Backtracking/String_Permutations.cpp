58af0a0db6d145ba2bcbde31659b6203220203bc
Shubham Pathak <pathakshubham1228@gmail.com>
2022-02-12 00:31:47 +0530

    String Permutations

---
+++ DSA Topics/Backtracking/String_Permutations.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void helper(string s, int idx,vector<string> &res)
+    {
+        if(idx==s.length()-1)
+        {
+            res.push_back(s);
+            return;
+            
+        } 
+
+    for(int i= idx;i<s.length();i++)
+    {
+        swap(s[i],s[idx]);
+        helper(s,idx+1,res);
+        swap(s[i],s[idx]);
+        
+    }
+
+}
+
+vector<string> permutation(string S)
+{
+        
+    vector<string>res;
+    helper(S,0,res);
+    return res;
+}
+int main()
+{
+    string s;
+    cin>>s;
+    vector<string>res;
+    res = permutation(s);
+
+    for(int i=0;i<res.size();i++)
+    {
+        cout<<res[i]<<" ";
+    }
+    return 0;
+}
