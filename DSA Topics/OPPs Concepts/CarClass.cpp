e775f22cfc85cec44c711b62d2509890daf2bdcb
pathakshubham21 <pathakshubham1228@gmail.com>
2024-07-26 23:03:15 +0530

    Added Code

--- DSA Topics/OPPs Concepts/CarClass.cpp
+++ DSA Topics/OPPs Concepts/CarClass.cpp
@@ -1,20 +1,27 @@
-Class Car
-{
-    public:
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Car {
+private:
+    int model;
+    int price; // Added this to fix the reference to price in Display()
+
+public:
     string name;
     int ManfacturedYear;
 
-    private:
-
-    int model;
-
-    public:
-
-    void Display()
-    {
-        cout<<"Car Name: "<<name;
-        cout<<"Car Price: "<<price;
-        cout<<"Car Manfactured Year: "<<ManfacturedYear;
+    void Display() {
+        cout << "Car Name: " << name << endl;
+        cout << "Car Price: " << price << endl;
+        cout << "Car Manufactured Year: " << ManfacturedYear << endl;
     }
 
-}
+    // Constructor to initialize the variables
+    Car(string carName, int carPrice, int carManufacturedYear) {
+        name = carName;
+        price = carPrice;
+        ManfacturedYear = carManufacturedYear;
+    }
+};
--- DSA Topics/OPPs Concepts/CarDeatils.cpp
+++ DSA Topics/OPPs Concepts/CarDeatils.cpp
@@ -1,9 +1,13 @@
-include<iostream>
-#include "CarClass.cpp"
-using namepsace std;
+#include <iostream>
+#include <string>
+#include "CarClass.cpp" // Including the Car class definition
 
+using namespace std;
 
-int main()
-{
+int main() {
+    // Creating an object of the Car class
+    Car myCar("Toyota", 20000, 2021);
+    myCar.Display();
     
+    return 0;
 }
