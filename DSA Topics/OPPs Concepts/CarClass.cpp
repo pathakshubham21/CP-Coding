6273ca8ce15b5d004350667302cbe838b93fe81c
pathakshubham21 <pathakshubham1228@gmail.com>
2024-07-26 15:05:05 +0530

    Added Code

---
+++ DSA Topics/OPPs Concepts/CarClass.cpp
@@ -0,0 +1,20 @@
+Class Car
+{
+    public:
+    string name;
+    int ManfacturedYear;
+
+    private:
+
+    int model;
+
+    public:
+
+    void Display()
+    {
+        cout<<"Car Name: "<<name;
+        cout<<"Car Price: "<<price;
+        cout<<"Car Manfactured Year: "<<ManfacturedYear;
+    }
+
+}
---
+++ DSA Topics/OPPs Concepts/CarDeatils.cpp
@@ -0,0 +1,9 @@
+include<iostream>
+#include "CarClass.cpp"
+using namepsace std;
+
+
+int main()
+{
+    
+}
