f4083c9e7b3ffe88baf0b5ec27d87f7144ddb531
pathakshubham21 <pathakshubham1228@gmail.com>
2024-08-16 23:51:27 +0530

    Added Code

--- DSA Topics/Recursion/subs.cpp
+++ DSA Topics/Recursion/subs.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
-int subs(string input, string output[]) {
-	if (input.empty()) {
-		output[0] = "";
-		return 1;
-	}
+// int subs(string input, string output[]) {
+// 	if (input.empty()) {
+// 		output[0] = "";
+// 		return 1;
+// 	}
 
-	string smallString = input.substr(1);
-	int smallOutputSize = subs(smallString, output);
-	for (int i = 0; i < smallOutputSize; i++) {
-		output[i + smallOutputSize] = input[0] + output[i];
-	}
-	return 2 * smallOutputSize;
+// 	string smallString = input.substr(1);
+// 	int smallOutputSize = subs(smallString, output);
+// 	for (int i = 0; i < smallOutputSize; i++) {
+// 		output[i + smallOutputSize] = input[0] + output[i];
+// 	}
+// 	return 2 * smallOutputSize;
+// }
+
+int subs(string input,string output,int size,int index)
+{
+    if(index==size)
+    {
+        cout<<output<<endl;
+        return 1;
+    }
+    int res=subs(input,output+input[index],size,index+1) + subs(input,output,size,index+1);
+    return res;
 }
 
 int main() {
-	string input;
-	cin >> input;
-	string* output = new string[1000];
-	int count = subs(input, output);
-	for (int i = 0; i < count; i++) {
-		cout << output[i] << endl;
-	}
+	string input= "ABC";
+    cout<<subs(input,"",input.length(),0);
+    return 0;
+	
 }
