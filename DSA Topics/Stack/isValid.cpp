5150a76d0804dccfcc99ea36b44b96e0fbb8667d
pathakshubham21 <pathakshubham1228@gmail.com>
2024-08-27 22:29:48 +0530

    Added Code

---
+++ DSA Topics/Stack/isValid.cpp
@@ -0,0 +1,42 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
+
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<int> a {1,2,3,4,5,6,7,8};
+        for (int i = 0; i < a.size() - 2; i++)
+            cout<<"GfG!";
+        return false;
+    }
+};
