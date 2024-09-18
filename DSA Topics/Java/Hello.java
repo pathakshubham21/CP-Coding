1a6376dcffab5060993a3f53a1ad71751d07dd0b
Shubham Pathak <52988876+pathakshubham21@users.noreply.github.com>
2024-09-18 23:03:26 +0530

    Update Hello.java

--- DSA Topics/Java/Hello.java
+++ DSA Topics/Java/Hello.java
@@ -2,6 +2,10 @@ class Hello
 {
     public static void main(String a[])
     {
+        int a = 5;
+        float b = 10.20f;
+        a=(float)b;
         System.out.print("Hello World");
+        System.out.print(a);
     }
 }
