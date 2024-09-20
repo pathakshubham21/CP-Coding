4f3d720abd93a8a3b3191652a26a5612b384928d
pathakshubham21 <pathakshubham1228@gmail.com>
2024-09-20 14:08:18 +0530

    Added Code

--- DSA Topics/Java/SwitchCase.java
+++ DSA Topics/Java/SwitchCase.java
@@ -14,3 +14,15 @@ switch (month) {
     case JANUARY, JUNE, JULY -> 3; 
     case FEBRUARY, SEPTEMBER -> 1;
 }
+
+// Using yield
+int x = switch (dayOfWeek) {
+    case MONDAY:
+        yield 2;
+    case TUESDAY:
+        yield 3;
+    case WEDNESDAY:
+        yield 4;
+    default:
+        yield 0;
+};
