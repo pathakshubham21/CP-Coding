04a6ba578a57d488bb3e48c11ebd924a408098e8
pathakshubham21 <pathakshubham1228@gmail.com>
2024-09-22 20:09:40 +0530

    Added Code

---
+++ DSA Topics/Array And 2DArray/pascal's_array.cpp
@@ -0,0 +1,53 @@
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+// Example 1:
+
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Example 2:
+
+// Input: numRows = 1
+// Output: [[1]]
+ 
+
+// Constraints:
+
+// 1 <= numRows <= 30
+
+
+class Solution {
+public:
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+    vector<vector<int>> generate(int n) {
+
+        vector<vector<int>> ans;
+
+    //Store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+        
+    }
+
+
+};
