25301ee85ed843d7679f3c13cbeddb17f06132e6
pathakshubham21 <pathakshubham1228@gmail.com>
2024-09-24 00:17:56 +0530

    Added Code

--- DSA Topics/Array And 2DArray/findDuplicateLinerSearch.cpp
+++ DSA Topics/Array And 2DArray/findDuplicateLinerSearch.cpp
@@ -31,7 +31,7 @@ class Solution {
 public:
 int findDuplicate(vector<int>& nums) {
     for (int i = 0; i < nums.size(); i++) {
-        int index = abs(nums[i]);  
+        int index = abs(nums[i]);  // Use the absolute value to get the index
         
         if (nums[index] < 0) { 
             return index;
