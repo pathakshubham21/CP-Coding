d5c34296c03abaec1dfef8470d8fa50732835dd8
pathakshubham21 <pathakshubham1228@gmail.com>
2024-10-08 21:29:52 +0530

    Added code

--- DSA Topics/Java/lern-spring_fremwork/src/main/java/com/FirstSpringBootProject/lern_spring_fremwork/LernSpringFremworkApplication.java
+++ DSA Topics/Java/lern-spring_fremwork/src/main/java/com/FirstSpringBootProject/lern_spring_fremwork/LernSpringFremworkApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.FirstSpringBootProject.lern_spring_fremwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class LernSpringFremworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LernSpringFremworkApplication.class, args);
 	}
 
 }
---
+++ DSA Topics/Java/lern-spring_fremwork/src/main/java/com/FirstSpringBootProject/lern_spring_fremwork/game/SuperContraGame.java
@@ -0,0 +1,21 @@
+package com.FirstSpringBootProject.lern_spring_fremwork.game;
+
+public class SuperContraGame implements GamingConsol {
+	public void up()
+	{
+		System.out.println("UP");
+	}
+	public void down()
+	{
+		System.out.println("Sitting down");
+	}
+	public void left()
+	{
+		System.out.println("Go back");
+	}
+	public void right()
+	{
+		System.out.println("Shooting buller");
+	}
+
+}
