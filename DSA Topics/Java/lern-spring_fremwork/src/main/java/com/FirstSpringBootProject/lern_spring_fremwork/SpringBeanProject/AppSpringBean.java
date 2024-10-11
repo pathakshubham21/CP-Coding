2f39da1e7b94d4d5c1bcc430b975484c4f877acc
pathakshubham21 <pathakshubham1228@gmail.com>
2024-10-11 12:15:42 +0530

    Added code

--- DSA Topics/Java/lern-spring_fremwork/src/main/java/com/FirstSpringBootProject/lern_spring_fremwork/SpringBeanProject/AppSpringBean.java
+++ DSA Topics/Java/lern-spring_fremwork/src/main/java/com/FirstSpringBootProject/lern_spring_fremwork/SpringBeanProject/AppSpringBean.java
@@ -12,6 +12,29 @@ public class AppSpringBean {
 		
 		// Get bean from Spring Context
 		System.out.println(Context.getBean("name"));
+		System.out.println(Context.getBean("age"));
+		
+		System.out.println(Context.getBean("person"));
+//		System.out.println(Context.getBean("address"));
+		
+		
+		//  Calling Bean by using Bean custom name --
+		System.out.println(Context.getBean("address2"));
+		
+		// Calling Bean by using Bean class name --
+		System.out.println(Context.getBean(Address.class));
+		
+		
+		//Calling person using method call 
+		System.out.println(Context.getBean("person2MethodCall"));
+		
+		//Calling person using parameter
+		System.out.println(Context.getBean("person3Parameter"));
+		
+		
+		Context.close();
+		
+		
 
 	}
 
