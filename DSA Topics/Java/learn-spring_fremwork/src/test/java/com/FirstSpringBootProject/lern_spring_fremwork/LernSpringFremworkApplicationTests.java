d8ccc068adf02e77077f4a95b400b3aa0e36a8f0
pathakshubham21 <pathakshubham1228@gmail.com>
2024-10-18 16:58:08 +0530

    Added code

---
+++ DSA Topics/Java/learn-spring_fremwork/src/test/java/com/FirstSpringBootProject/lern_spring_fremwork/LernSpringFremworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FirstSpringBootProject.lern_spring_fremwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernSpringFremworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
