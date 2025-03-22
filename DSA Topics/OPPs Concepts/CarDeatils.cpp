6344d28b42232940dc057ab26cbb2e469083654f
pathakshubham21 <shubhampath@adobe.com>
2025-03-22 22:16:49 +0530

    Checking chiito

--- DSA Topics/OPPs Concepts/CarDeatils.cpp
+++ DSA Topics/OPPs Concepts/CarDeatils.cpp
@@ -4,6 +4,37 @@
 
 using namespace std;
 
+void checking()
+{
+ vector<int>vec(2,5, 6, 11, 15);
+ int x = 11;
+ 
+ int l, r, mid;
+
+ while(l<=r)
+ {
+    mid= (l+r)/2;
+
+    if(vec[mid]==x)
+    {
+        cout<<"Found at "<<mid<<;
+        break;
+    }
+    else if(vec[mid]>x)
+    {
+        r= mid -1;
+
+    }
+    else 
+    {
+        l = mid+1;
+    }
+
+}
+
+return;
+}
+
 int main() {
     // Creating an object of the Car class
     Car myCar1;
